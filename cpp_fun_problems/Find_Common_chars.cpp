e09fb70abee6ab1870936e05464595a40701fa20
Kiran.Kasichayanula <Kiran.Kasichayanula@PRL-DL-PRLOAN04>
2019-06-06 17:53:00 -0400

    added common chars

--- cpp_fun_problems/Find_Common_chars.cpp
+++ cpp_fun_problems/Find_Common_chars.cpp
@@ -1,37 +1,44 @@
 #include<iostream>
 #include<vector>
 #include<map>
+#include <algorithm>
+
 
 using namespace std;
 
 class Solution {
 public:
 	vector<string> commonChars(vector<string>& A) {
-		int tmp_arr[27]{ 0 };
-		vector<string> ret_vt;
-		for (int i = 0; i < A.size(); i++) {
-			
-			string tmp = A[i];
-			for (int j = 0; j < tmp.length(); j++) {
-				char x = tmp[j];
-				int k = (int)x;
-				tmp_arr[k - 97] += 1;
+		if (A.empty()) {
+			return {};
+		}
+		int count[26] = { 0 };
+		for (char ch : A[0]) {
+			count[ch - 'a']++;
+		}
+		for (int i = 1; i < A.size(); i++) {
+			int c[26] = { 0 };
+			for (auto ch : A[i]) {
+				c[ch - 'a']++;
+			}
+			for (int i = 0; i < 26; i++) {
+				count[i] = min(count[i], c[i]);
 			}
 		}
-		for (int j = 0; j < 26; j++) {
-			if (tmp_arr[j] == A.size()) {
-				string j_tmp = "";
-				j_tmp += char(j + 97);
-				ret_vt.push_back(j_tmp);
+		vector<string> ret;
+		for (int i = 0; i < 26; i++) {
+			while (count[i]-- > 0) {
+				char ch = static_cast<char>('a' + i);
+				ret.push_back(string(1, ch));
 			}
 		}
-		return ret_vt;
+		return ret;
 	}
 };
 
 int main() {
 	Solution mysol;
-	vector<string> arr{ "abc", "bca", "cab" };
+	vector<string> arr{ "kk", "kkaa", "xakkab" };
 	vector<string> ret_vt = mysol.commonChars(arr);
 	for (int i = 0; i < ret_vt.size(); i++) {
 		cout << ret_vt[i] << endl;
